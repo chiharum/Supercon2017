b7b6a1a752d848c6c272337b19ee7bb80876bafe
chiharum <m.chiharu0611@gmail.com>
2017-06-16 02:31:16 +0900

    made secondbranch

---
+++ SCcrossing.java
@@ -0,0 +1,15 @@
+public class SCcrossing{
+
+	int n;
+	
+	public static void main (String[] args) throws IOException{
+
+		InputStreamReader inputStreamReader  = new InputStreamReader(System.in);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+		String nInput = bufferedReader.readLine();
+		n = Integer.parseInt(nInput);
+
+		// test
+	}
+}
